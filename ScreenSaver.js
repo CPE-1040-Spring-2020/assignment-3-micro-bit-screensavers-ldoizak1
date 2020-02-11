a17cffce36359b29d97f7927480690befaa10682
ldoizak1 <47997282+ldoizak1@users.noreply.github.com>
2020-02-10 19:39:36 -0700

    Update and rename ScreenSaver to ScreenSaver.js

--- ScreenSaver.js
+++ ScreenSaver.js
@@ -33,7 +33,7 @@ let myImage5 = images.createImage(`
 # # # # # # # # # #
 # . . # # # # . . #
 `)
-let awake: boolean = false
+let awake: boolean = true
 let screenFoos = [screen1, screen2, screen3, screen4, screen5]
 let gestures = [Gesture.Shake, Gesture.LogoDown, Gesture.LogoUp, Gesture.TiltLeft,
 Gesture.TiltRight]
@@ -56,56 +56,48 @@ input.onGesture(Gesture.TiltRight, function () {
     gest = Gesture.TiltRight
 })
 function screen1() {
-    for (let y = 0; y < 3; y++) {
-        for (let x = 0; x < 6; x++) {
-            myImage1.showImage(x)
-        }
-        basic.clearScreen()
+    for (let x = 0; x < 6; x++) {
+        myImage1.showImage(x)
     }
+    basic.clearScreen()
 }
 function screen2() {
-    for (let y = 0; y < 3; y++) {
-        for (let x = 0; x < 6; x++) {
-            myImage2.showImage(x)
-        }
-        basic.clearScreen()
+    for (let x = 0; x < 6; x++) {
+        myImage2.showImage(x)
     }
+    basic.clearScreen()
 }
 function screen3() {
-    for (let y = 0; y < 3; y++) {
-        for (let x = 0; x < 6; x++) {
-            myImage3.showImage(x)
-        }
-        basic.clearScreen()
+    for (let x = 0; x < 6; x++) {
+        myImage3.showImage(x)
     }
+    basic.clearScreen()
 }
 function screen4() {
-    for (let y = 0; y < 3; y++) {
-        for (let x = 0; x < 5; x++) {
-            myImage4.showImage(x)
-        }
-        basic.clearScreen()
+    for (let x = 0; x < 5; x++) {
+        myImage4.showImage(x)
     }
+    basic.clearScreen()
 }
 function screen5() {
-    for (let y = 0; y < 3; y++) {
-        for (let x = 0; x < 6; x++) {
-            myImage5.showImage(x)
-        } basic.clearScreen()
+    for (let x = 0; x < 6; x++) {
+        myImage5.showImage(x)
     }
+    basic.clearScreen()
 }
-input.onButtonPressed(Button.A, function () {
-    awake = !awake
+input.onButtonPressed(Button.B, function () {
+    awake = true
 })
-
 
 
 basic.forever(function () {
-    if (awake) {
-        basic.showString("Hello! Do you want to play?")
 
+    if (awake) {
+        basic.showString("Hello!")
+        awake = !awake
     } else {
-        screenFoos[gestures.indexOf(gest)]()
+        input.onButtonPressed(Button.A, function () {
+            screenFoos[gestures.indexOf(gest)]()
+        })
     }
 })
-
